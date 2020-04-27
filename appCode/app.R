d6081cfc47701fad2720899a93b62137c2a73bba
JWVivs <48459571+JWVivs@users.noreply.github.com>
2020-04-27 17:47:33 -0400

    added leaflet map to ui

--- appCode/app.R
+++ appCode/app.R
@@ -27,6 +27,12 @@ charleston_final <- read.csv("C:/Users/JVivs/Documents/COLLEGE/GRAD SCHOOL/Capst
 # binding df_boston and df_portland (contains restaurant info)
 df_combined <- rbind(df_boston, df_portland)
 
+# reading in restaurant info
+df_3 <- read.csv("./data/processed/df_3.csv")
+# reading in the coordinate data
+df_3_coord <- read.csv("./data/processed/df_3_coord.csv")
+
+### 1st design for app ###
 
 # Define UI for application that draws a histogram
 ui <- dashboardPage(skin = "red",
@@ -62,33 +68,46 @@ ui <- dashboardPage(skin = "red",
 # Preview the UI in the console
 shinyApp(ui = ui, server = function(input, output) { })
 
+##########################
 
-# Define server logic required to draw a histogram
+### 2nd design for app ###
+
+ui<- dashboardPage(
+    skin = "red",
+    dashboardHeader(title = "Restaurant App"),
+    
+    # Sidebar content
+    dashboardSidebar(
+        sidebarMenu(
+            menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+            menuItem("App Info", tabName = "appinfo", icon = icon("info-circle")),
+        selectInput("City",label = "City",
+                    choices = df_3_coord$City)
+    )),
+    
+    # Body content
+    dashboardBody(
+        fluidRow(box(width = 12, leafletOutput(outputId = "mymap", height = 875)))
+    ))
+
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    
+    output$mymap<- renderLeaflet({
+        validate(need(df_3_coord,"Add file"))
+        validate(need(df_3_coord$City,"Select Area"))
+        df_3_coord %>%
+            leaflet() %>%
+            addTiles() %>%
+            addMarkers(clusterOptions = markerClusterOptions(), 
+                       popup = paste(df_3$Restaurant, "<br>",
+                                     df_3$Cuisine, "<br>",
+                                     df_3$Address))
     })
 }
 
-# Run the application 
 shinyApp(ui = ui, server = server)
 
 
 
-### Body contentbckup
-dashboardBody(
-    fluidRow(
-        box(plotOutput("map")),
-        
-        box(
-            "Box content here", br(), "More box content",
-            selectInput("select", "Select a City:", c("Boston", "Portland", "Charleston"))
-        )
-    )
-))
+
+
